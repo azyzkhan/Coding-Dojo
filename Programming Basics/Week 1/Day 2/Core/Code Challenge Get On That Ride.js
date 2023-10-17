3fec2c78b1807e4504bc5b53181accdfddf94aea
azyzkhan <boywiser4@gmail.com>
2023-10-17 18:17:54 +0100

    commit

---
+++ Programming Basics/Week 1/Day 2/Core/Code Challenge Get On That Ride.js
@@ -0,0 +1,9 @@
+var heightMeasurement = 42
+if (heightMeasurement = 41) {
+    console.log( "Sorry kiddo. Maybe next year.");
+}
+else if (heightMeasurementAndAge = 42) {
+    console.log("Get on that ride, kiddo!");
+}
+
+// not completed //
---
+++ Programming Basics/Week 1/Day 2/Practice/Practice Commenting.js
@@ -0,0 +1,7 @@
+// It's a var named likeCount and it equal to 0 this var likeCount will tell us how many like counts we have. - what would this variable do?
+var likeCount = 0;
+// The function is called increase like which will help us to increase the likeCount. - what would be the function of this code block?
+function increaseLikes() {
+    // Here we are changing the value of likeCount - what is this sort operation for?
+    likeCount = likeCount + 1;
+}
